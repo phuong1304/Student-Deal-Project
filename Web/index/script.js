55e1f361fb56a62e87534b4e124e2f2d7c1df1a3
Bich Phuong <bichphuong13042002@gmail.com>
2023-05-24 23:51:39 +0700

    add promo

---
+++ Web/index/script.js
@@ -0,0 +1,27 @@
+// Tạo chuyển động qua lại trên promo 
+var ImgPromo = 1;
+showDivs(ImgPromo);
+
+function plusDivs(n) {
+    showDivs(ImgPromo += n);
+}
+
+function currentDiv(n) {
+    showDivs(ImgPromo = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("imgP");
+    var dots = document.getElementsByClassName("image-badge");
+    if (n > x.length) { ImgPromo = 1 }
+    if (n < 1) { ImgPromo = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" badge-white", "");
+    }
+    x[ImgPromo - 1].style.display = "block";
+    dots[ImgPromo - 1].className += " badge-white";
+}
