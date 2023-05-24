231ff1d7f02e5c00d482e71f519a887ca8bb741a
Bich Phuong <bichphuong13042002@gmail.com>
2023-05-24 15:16:55 +0700

    add all

---
+++ Web/login/login.js
@@ -0,0 +1,15 @@
+const formContainer = document.querySelector('.form-container');
+const loginForm = document.querySelector('#sign-in-form');
+const registerForm = document.querySelector('#sign-up-form')
+
+const switchForm = (form) => {
+    if (form == 'register') {
+        formContainer.style.left = '50%';
+        loginForm.style.marginLeft = '-150%';
+        registerForm.style.marginLeft = '-100%';
+    } else {
+        formContainer.style.left = '0%';
+        loginForm.style.marginLeft = '0%';
+        registerForm.style.marginLeft = '50%';
+    }
+}
